a94811bb8e7ff04a0c3070a3eb272e054b3dcad0
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2017-01-09 04:42:46 -0500

    redesigned entire app interface + updateWaterConsumption

--- app/src/main/java/com/example/anubhabmajumdar/hydrationapp/Main2Activity.java
+++ app/src/main/java/com/example/anubhabmajumdar/hydrationapp/Main2Activity.java
@@ -1,9 +1,12 @@
 package com.example.anubhabmajumdar.hydrationapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,9 +14,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.anubhabmajumdar.hydrationapp.R.id.chart;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    int notification_interval, glass_size, totalWaterConsumption;
+    double quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +38,6 @@ public class Main2Activity extends AppCompatActivity
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,6 +47,32 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        extractSettingsData();
+        setUpPieChart();
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        extractSettingsData();
+        setUpPieChart();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        saveTotalWaterConsumption();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        saveTotalWaterConsumption();
     }
 
     @Override
@@ -51,12 +85,6 @@ public class Main2Activity extends AppCompatActivity
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main2, menu);
-//        return true;
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -90,11 +118,87 @@ public class Main2Activity extends AppCompatActivity
 
         else if (id == R.id.action_settings)
         {
-
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+/* --------------------------------------------- myFunctions() ------------------------------------------------------------------ */
+
+    public void extractSettingsData()
+    {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        this.notification_interval = Integer.parseInt(sharedPref.getString(getString(R.string.notification_interval), getString(R.string.notification_default)));
+        this.quantity = Double.parseDouble(sharedPref.getString(getString(R.string.quantity), getString(R.string.quantity_default)));
+        this.glass_size = Integer.parseInt(sharedPref.getString(getString(R.string.glass_size), getString(R.string.glass_size_default)));
+        this.totalWaterConsumption = Integer.parseInt(sharedPref.getString(getString(R.string.total_consumption), getString(R.string.totalWaterConsumption_default)));
+
+        showToast(Integer.toString(notification_interval));
+    }
+
+    public void saveTotalWaterConsumption()
+    {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(getString(R.string.total_consumption), Integer.toString(totalWaterConsumption));
+        editor.apply();
+    }
+
+    public void showToast(String text)
+    {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void setUpPieChart()
+    {
+        PieChart pieChart = (PieChart) findViewById(chart);
+        if (pieChart != null)
+        {
+            pieChart.getDescription().setEnabled(false);
+            pieChart.getLegend().setEnabled(false);
+            pieChart.setTransparentCircleColor(Color.WHITE);
+            //pieChart.animateX(2000);
+            pieChart.animateY(1000);
+
+            List<PieEntry> entries = new ArrayList<>();
+
+            entries.add(new PieEntry(totalWaterConsumption, "Water Consumed"));
+            int remaining = Math.max(0, ((int) (quantity * 1000) - totalWaterConsumption));
+            entries.add(new PieEntry(remaining, "Remaining"));
+
+            PieDataSet set = new PieDataSet(entries, "Water Consumption");
+            int color_green = getResources().getColor(R.color.darkgreen);
+            int color_blue = getResources().getColor(R.color.darkblue);
+
+            set.setColors(new int[]{color_blue, color_green});
+            set.setValueTextSize(25);
+
+            PieData data = new PieData(set);
+            pieChart.setData(data);
+
+            pieChart.invalidate(); // refresh
+        }
+    }
+
+    public void updateWaterConsumption(View v)
+    {
+        totalWaterConsumption = totalWaterConsumption + glass_size;
+
+        SharedPreferences sharedPref = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putInt(getString(R.string.total_consumption), totalWaterConsumption);
+        editor.apply();
+
+        setUpPieChart();
     }
 }
--- app/src/main/java/com/example/anubhabmajumdar/hydrationapp/TimePickerFragment.java
+++ app/src/main/java/com/example/anubhabmajumdar/hydrationapp/TimePickerFragment.java
@@ -30,8 +30,9 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         // Do something with the time chosen by the user
-        HydrationSettingActivity ac = ((HydrationSettingActivity)getActivity());
-        ac.setTime(hourOfDay, minute);
+        String time=String.valueOf(hourOfDay)+":"+String.valueOf(minute);
+
+
     }
 
 }
